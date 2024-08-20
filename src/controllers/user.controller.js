4a88bf0fb9e5a01b246825fd4d048a1551e0c3be
Lucas-Rodrigues-code <Rodriguesdesouzal28@gmail.com>
2024-08-20 10:28:18 -0300

    feat : try and catch in controllers, middlewares

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,60 +1,80 @@
 const userServices = require("../services/user.service");
 
 async function create(req, res) {
-  const { name, email, password, userName, avatar, background } = req.body;
+  try {
+    const { name, email, password, userName, avatar, background } = req.body;
 
-  if (!name || !userName || !email || !password || !avatar || !background) {
-    return res.status(400).send("Missing required fields for user creation");
+    if (!name || !userName || !email || !password || !avatar || !background) {
+      return res.status(400).send("Missing required fields for user creation");
+    }
+
+    if (
+      !email
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+    ) {
+      return res.status(400).send("Invalid email");
+    }
+
+    const user = await userServices.create(req.body);
+    if (!user) return res.status(400).send("Error creating user");
+
+    res.send({ user });
+  } catch (error) {
+    console.error(error, "Error creating user");
+    res.status(500).send("Internal server error");
   }
-
-  if (
-    !email
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      )
-  ) {
-    return res.status(400).send("Invalid email");
-  }
-
-  const user = await userServices.create(req.body);
-  if (!user) return res.status(400).send("Error creating user");
-
-  res.send({ user });
 }
 
 async function getAll(req, res) {
-  const users = await userServices.getAll();
-  if (!users) return res.status(400).send("Users not found");
+  try {
+    const users = await userServices.getAll();
+    if (!users) return res.status(400).send("Users not found");
 
-  res.send(users);
+    res.send(users);
+  } catch (error) {
+    console.error(error, "Error getting all users");
+    res.status(500).send("Internal server error");
+  }
 }
 
 async function getUsersById(req, res) {
-  const { user } = req;
+  try {
+    const { user } = req;
 
-  res.send(user);
+    res.send(user);
+  } catch (error) {
+    console.error(error, "Error getting user by id");
+    res.status(500).send("Internal server error");
+  }
 }
 
 async function update(req, res) {
-  const { name, email, password, userName, avatar, background } = req.body;
-  
-  if (!name && !userName && !email && !password && !avatar && !background) {
-    return res.status(400).send("one or more fields are required");
-  }
+  try {
+    const { name, email, password, userName, avatar, background } = req.body;
 
-  const { id } = req;
-  
-  await userServices.updateService({
-    id,
-    name,
-    email,
-    password,
-    userName,
-    avatar,
-    background,
-  });
-  res.send({ message: "User updated" });
+    if (!name && !userName && !email && !password && !avatar && !background) {
+      return res.status(400).send("one or more fields are required");
+    }
+
+    const { id } = req;
+
+    await userServices.updateService({
+      id,
+      name,
+      email,
+      password,
+      userName,
+      avatar,
+      background,
+    });
+    res.send({ message: "User updated" });
+  } catch (error) {
+    console.error(error, "Error updating user");
+    res.status(500).send("Internal server error");
+  }
 }
 
 module.exports = { create, getAll, getUsersById, update };
--- src/middlewares/global.middlewares.js
+++ src/middlewares/global.middlewares.js
@@ -2,23 +2,33 @@ const mongoose = require("mongoose");
 const userServices = require("../services/user.service");
 
 function validId(req, res, next) {
-  const { id } = req.params;
-  if (!id || !mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ message: "Invalid ID" });
+  try {
+    const { id } = req.params;
+    if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "Invalid ID" });
+    }
+    next();
+  } catch (error) {
+    console.error(error, "Error validating id");
+    res.status(500).send("Internal server error");
   }
-  next();
 }
 
 async function validUser(req, res, next) {
-  const { id } = req.params;
-  const user = await userServices.getUsersById(id);
-  if (!user) {
-    return res.status(400).json({ message: "User not found" });
-  }
+  try {
+    const { id } = req.params;
+    const user = await userServices.getUsersById(id);
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
 
-  req.id = id;
-  req.user = user;
-  next();
+    req.id = id;
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error(error, "Error validating user");
+    res.status(500).send("Internal server error");
+  }
 }
 
 module.exports = { validId, validUser };
