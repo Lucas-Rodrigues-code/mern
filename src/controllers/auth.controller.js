0e462b709e6b1f14cfccb73b33e4e5e2c08eb815
Lucas-Rodrigues-code <Rodriguesdesouzal28@gmail.com>
2024-08-21 11:09:47 -0300

    feat: Add JWT token generation for login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import { findUserByEmail } from "../services/auth.service.js";
+import { findUserByEmail, generateToken } from "../services/auth.service.js";
 
 async function login(req, res) {
   try {
@@ -12,7 +12,10 @@ async function login(req, res) {
     if (!isValid) {
       return res.status(400).json({ message: "User or password not found" });
     }
-    res.send("ok");
+
+    const token = generateToken(user);
+
+    res.send({ token });
   } catch (error) {
     console.error(error, "Error logging in");
     res.status(500).send("Internal server error");
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,16 @@
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 function findUserByEmail(email) {
   return User.findOne({ email }).select("+password");
 }
 
-export { findUserByEmail };
+function generateToken(user) {
+  return jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+}
+export { findUserByEmail, generateToken };
