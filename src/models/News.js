632801fd1627745d1152996767130f407b56459d
Lucas-Rodrigues-code <Rodriguesdesouzal28@gmail.com>
2024-08-21 11:25:29 -0300

    feat: Add News model for storing news articles

---
+++ src/models/News.js
@@ -0,0 +1,39 @@
+import req from "express/lib/request";
+import { type } from "express/lib/response";
+import mongoose from "mongoose";
+
+const NewsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  banner: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  likes: {
+    type: Array,
+    required: true,
+  },
+  comments: {
+    type: Array,
+    required: true,
+  },
+});
+
+const News = mongoose.model("News", NewsSchema);
+
+export default News;
